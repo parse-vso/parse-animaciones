054758d97ad9c1eedb49eeb3a573c111ab1a77c8
Parsero <84363195+parse-vso@users.noreply.github.com>
2021-05-18 16:24:36 +0200

    Add files via upload

---
+++ parse-anims/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Manifiesto
+fx_version 'cerulean'
+
+game 'gta5'
+
+-- Filas Clientes
+client_scripts {
+	'client/agacharse.lua',
+	'client/anticulatazos.lua',
+	'client/senalar.lua',
+	'client/handsup.lua',
+}
